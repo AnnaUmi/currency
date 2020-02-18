b0d76cbe11aa5f55b9b4b78104c87a733a155bbd
annaumi <anna.a.vlasenko@yandex.ua>
2020-02-18 10:07:51 +0200

    add calc rate func

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function calc() {
-  fetch("items.json")
+const currencyFrom = document.getElementById("currency-one");
+const currencyTo = document.getElementById("currency-two");
+const amountFrom = document.getElementById("amount-one");
+const amountTo = document.getElementById("amount-two");
+const rate = document.getElementById("rate");
+const swap = document.getElementById("swap");
+
+//Фетчит обмен валют и обновляет дои
+
+//Обработчики событий
+currencyFrom.addEventListener("change", calculate);
+currencyTo.addEventListener("input", calculate);
+amountFrom.addEventListener("change", calculate);
+amountTo.addEventListener("input", calculate);
+
+function calculate() {
+  const currencyValueFrom = currencyFrom.value;
+  const currencyValueTo = currencyTo.value;
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currencyValueFrom}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+      //console.log("data", data);
+      const rateValue = data.rates[currencyValueTo];
+      //console.log("rate", rate);
+      rate.innerText = `1 ${currencyValueFrom} = ${rateValue} ${currencyValueTo}`;
+      amountTo.value = (amountFrom.value * rateValue).toFixed(2);
+    });
 }
-calc();
+swap.addEventListener("click", () => {
+  const temp = currencyFrom.value;
+  currencyFrom.value = currencyTo.value;
+  currencyTo.value = temp;
+  calculate();
+});
